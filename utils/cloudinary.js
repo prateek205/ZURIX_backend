04b33355680b566a288a76c6a315ef178a25a4ab
Prateek Bahad <prateekbahad@gmail.com>
2026-09-14 15:52:27 +0530

    update the cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,5 +1,12 @@
 import { v2 as cloudinary } from "cloudinary";
 
+console.log("CLOUD NAME:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API KEY:", process.env.CLOUDINARY_API_KEY);
+console.log(
+  "API SECRET EXISTS:",
+  !!process.env.CLOUDINARY_API_SECRET
+);
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
