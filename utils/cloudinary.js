1c4667f35b87918cdda8e2c0315a759194e07978
Prateek Bahad <prateekbahad@gmail.com>
2026-09-14 16:01:17 +0530

    update the cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,7 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
-import dotenv from "dotenv"
 
-dotenv.config()
 
 console.log("CLOUD NAME:", process.env.CLOUDINARY_CLOUD_NAME);
 console.log("API KEY:", process.env.CLOUDINARY_API_KEY);
