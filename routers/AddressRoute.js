d2d3f555c9d2dd2287e3a3abb40ec41ec2b260f9
Prateek Bahad <prateekbahad@gmail.com>
2026-09-22 14:58:36 +0530

    update the addressRoute.js

--- routers/AddressRoute.js
+++ routers/AddressRoute.js
@@ -2,7 +2,7 @@ import express from "express"
 import { protectedRoute } from "../middleware/AuthMiddleware.js"
 import { createAddress } from "../controllers/AddressController.js"
 
-const router = express.router()
+const router = express.Router()
 
 
 router.post("/createAddress", protectedRoute, createAddress)
