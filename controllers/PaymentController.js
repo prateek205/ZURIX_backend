82da3731273b0bc1e9a6b7199789bc4b406327d5
Prateek Bahad <prateekbahad@gmail.com>
2026-09-26 09:41:16 +0530

    update the paymentController.js

--- controllers/PaymentController.js
+++ controllers/PaymentController.js
@@ -61,9 +61,9 @@ export const createRazorpayOrder = async (req, res) => {
       subtotal += itemTotal;
     }
 
-    const shippingCharges = sellingPrice >= 1000 ? 0 : 100;
+    const shippingCharges = subtotal >= 1000 ? 0 : 100;
 
-    const totalAmount = sellingPrice + shippingCharges;
+    const totalAmount = subtotal + shippingCharges;
 
     console.log("SUBTOTAL:", subtotal);
     console.log("SHIPPING:", shippingCharges);
