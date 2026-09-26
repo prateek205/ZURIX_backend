67d1945fe7278f2ad1d3e5458d7dfd44b7b5b206
Prateek Bahad <prateekbahad@gmail.com>
2026-09-26 16:12:43 +0530

    update the wishlistRouter.js

--- routers/WishlistRouter.js
+++ routers/WishlistRouter.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { protectedRoute } from "../middleware/AuthMiddleware.js";
-import { createWishlist, getAllWishlist } from "../controllers/WishlistController.js";
+import { createWishlist, getAllWishlist, updateWishlist } from "../controllers/WishlistController.js";
 
 const router = express.Router()
 
 router.post("/createWishlist", protectedRoute, createWishlist)
 router.get("/getAllWishlist", protectedRoute, getAllWishlist)
+router.put("/updateWishlist/:id", protectedRoute, updateWishlist)
 
 export default router;
